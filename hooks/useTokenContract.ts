6abf5b8bd7b421d3e32c616bd005661fbef642a6
georgi-l95 <glazarov95@gmail.com>
2022-07-28 16:21:20 +0300

    advanced-ethers

--- hooks/useTokenContract.ts
+++ hooks/useTokenContract.ts
@@ -1,7 +1,11 @@
+import { ethers } from "ethers";
 import LIB_ABI from "../contracts/LIB.json";
 import type { LIB } from "../contracts/types";
 import useContract from "./useContract";
 
 export default function useTokenContract(contractAddress?: string) {
+  if (!ethers.utils.isAddress(contractAddress)) {
+    throw new Error("Token contract address is incorrect!");
+  }
   return useContract<LIB>(contractAddress, LIB_ABI);
 }
--- hooks/useWraperContract.ts
+++ hooks/useWraperContract.ts
@@ -1,7 +1,11 @@
+import { ethers } from "ethers";
 import LIBWRAPPER_ABI from "../contracts/LIBWrapper.json";
 import type { LIBWrapper } from "../contracts/types";
 import useContract from "./useContract";
 
 export default function useWrapperContract(contractAddress?: string) {
+  if (!ethers.utils.isAddress(contractAddress)) {
+    throw new Error("Token wrapper contract address is incorrect!");
+  }
   return useContract<LIBWrapper>(contractAddress, LIBWRAPPER_ABI);
 }
